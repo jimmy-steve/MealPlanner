334a477150e2703ddd00048d6bc8c35ed6d1da10
GG <gaellego@hotmail.com>
2023-02-27 22:17:10 -0500

    Recipe Card - axios

--- src/components/RecipeCard.js
+++ src/components/RecipeCard.js
@@ -3,26 +3,44 @@ import axios from "axios";
 import "./RecipeCard.scss";
 
 function RecipeCard() {
-    return(        
-        <div className="border rounded m-3 card--container">
+  const [mealList, setMealList] = React.useState([]);
+
+  const fetchMealListWithIngredient = () => {
+    axios.get("http://localhost:8000/api/Recipes").then((response) => {
+      setMealList(response.data);
+    });
+  };
+
+  React.useEffect(() => {
+    fetchMealListWithIngredient();
+  }, []);
+
+  return (
+    <div>
+      {mealList.map((meal) => (
+        <div key={meal.id}>
+          <div className="border rounded m-3 card--container">
             <div className="card--img--container">
-                <img src="./image1.jpg" alt="plat" className="card--img"></img>
+              <img src="./image1.jpg" alt="meal" className="card--img" />
             </div>
             <div className="card--text--container">
-                <h4>Title</h4>
-                <div className="row">
-                    <div className="col">
-                        <span class="material-symbols-outlined">av_timer</span>
-                        <span className="align-top m-1">25 min</span>
-                    </div>
-                    <div className="col">
-                        <span class="material-symbols-outlined">cooking</span>
-                        <span className="align-top m-1">40 min</span>
-                    </div>
-                </div>      
-            </div>  
+              <h4>{meal.title}</h4>
+              <div className="row">
+                <div className="col">
+                  <span className="material-symbols-outlined">av_timer</span>
+                  <span className="align-top m-1">{meal.preparationTime} min</span>
+                </div>
+                <div className="col">
+                  <span className="material-symbols-outlined">cooking</span>
+                  <span className="align-top m-1">{meal.cookingTime} min</span>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-    )
+      ))}
+    </div>
+  );
 }
 
 export default RecipeCard;
