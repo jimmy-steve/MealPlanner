6e6411ff51dbbc853c85a94f29ccf2c22e164100
GG <gaellego@hotmail.com>
2023-03-05 09:01:57 -0500

    RecipeList - corrections mineures

--- src/components/recipesList/RecipeAdd.js
+++ src/components/recipesList/RecipeAdd.js
@@ -5,7 +5,7 @@ import "./RecipeAdd.scss";
 function RecipeAdd() {
     return(
         <div>
-            <div className="m-3">
+            <div className="mt-5">
                     <button id="addRecipe" name="addRecipe" type="button" className="btn btn-secondary">Ajouter une recette</button>
             </div> 
         </div>
